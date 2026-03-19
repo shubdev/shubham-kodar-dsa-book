e41df7c3aacd694c34127e33814ef6bd2e8c865a
shubdev <shub.mahadule@gmail.com>
2026-03-20 03:16:06 +0530

    solve que 10

---
+++ Que10.js
@@ -0,0 +1,9 @@
+//Find surface area of sphere
+
+let radius = Number(prompt("Enter radius:"));
+
+//formula , pi=3.14
+let surfaceArea = 4 * Math.PI * Math.pow(radius, 2);
+
+console.log(`Surface area of sphere = ${surfaceArea}`);
+alert(`Surface area of sphere = ${surfaceArea}`);
