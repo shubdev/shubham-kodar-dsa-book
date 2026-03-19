6de246512a5ae40774f6a46a46c2fe99fbae2cf5
shubdev <shub.mahadule@gmail.com>
2026-03-20 02:34:07 +0530

    solve que 5

---
+++ Que5.js
@@ -0,0 +1,17 @@
+//Solve Increment & decrement operator questions
+
+let a = 5;
+
+//increment
+let result = a++
+console.log(result);
+console.log(a);
+
+//decrement
+let b = 4;
+let dec = b--
+console.log(dec);
+console.log(b);
+
+
+
