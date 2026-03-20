9df0a54f090b9b668203a52e1725d86003b90319
shubdev <shub.mahadule@gmail.com>
2026-03-20 18:02:09 +0530

    solve que16

--- Que16.js
+++ Que16.js
@@ -1,4 +1,18 @@
 //Accept name and age from the user. Check if the user is a valid voter or not. (Valid - Hello Shery, You are a valid voter. Invalid - Sorry Shery, you can't cast the vote. Part 2 - Print after how many years the user will be eligible)
 
 
-let 
+let userName = prompt("Enter Name");
+let age = Number(prompt("Enter Age"));
+
+
+const validVoter = () => {
+
+    if (age > 18) {
+        console.log(`Hello ${userName}, You are a valid voter.`);
+    }
+    else {
+        console.log(`Sorry ${userName}, you can't cast the vote After 18 you can vote.`);
+    }
+
+}
+validVoter();
