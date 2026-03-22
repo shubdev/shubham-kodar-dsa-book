0e1f6ebd55b3f9e0552f95cc7902725279e156eb
shubdev <shub.mahadule@gmail.com>
2026-03-22 23:36:53 +0530

    solve que 27

---
+++ Que27.js
@@ -0,0 +1,17 @@
+//Sum up to n terms. 
+//program to find the sum of first n natural numbers:
+
+let num = Number(prompt("Enter number"));
+let sum = 0;
+
+const sumOfNumber = () => {
+    if (num > 0) {
+        for (let i = 1; i <= num; i++) {
+            sum = sum + i;
+        }
+        console.log("sum of n numbers=", sum);
+    }
+
+}
+sumOfNumber();
+console.log("sum=", sum);
