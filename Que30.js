5f72cb25fe5e5a7c0d454e4b58a4e3b014394495
shubdev <shub.mahadule@gmail.com>
2026-03-29 16:32:52 +0530

    solved

--- Que30.js
+++ Que30.js
@@ -3,10 +3,11 @@
 function printFactors(n) {
   for (let i = 1; i <= n; i++) {
     if (n % i === 0) {
+      // if n is divisible by i then i is a factor of n
       console.log(i);
     }
   }
 }
 
 // Example
-printFactors(12);
+printFactors(12); //1,2,3,4,6,12
