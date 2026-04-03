ac27548308efe4ab6198801c32397bed772d9e45
shubdev <shub.mahadule@gmail.com>
2026-04-03 23:20:54 +0530

    solve que

---
+++ Que37.js
@@ -0,0 +1,14 @@
+//Sum of digits of a number (Ex: 936 = 18)
+
+function sumOfDigits(num) {
+    let str = num.toString();
+    let sum = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        sum += Number(str[i]);
+    }
+
+    return sum;
+}
+
+console.log(sumOfDigits(936));
