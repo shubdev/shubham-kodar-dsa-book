86b86017f9ce78f39837bdf12d3d0976e194d547
shubdev <shub.mahadule@gmail.com>
2026-04-07 00:00:45 +0530

    solved que

--- Que41.js
+++ Que41.js
@@ -3,16 +3,15 @@
 
 
 function isAutomorphic(n) {
-  let square = n * n;
+    let square = n * n;
 
-  // convert to string
-  let numStr = n.toString();
-  let squareStr = square.toString();
 
-  return squareStr.endsWith(numStr);
+    let numStr = n.toString();    // convert to string
+    let squareStr = square.toString();
+
+    return squareStr.endsWith(numStr);
 }
 
-// Test
 console.log(isAutomorphic(5));   // true
 console.log(isAutomorphic(6));   // true
 console.log(isAutomorphic(76));  // true
