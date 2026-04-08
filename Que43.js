85c4ea2f9ad428e08d1dfc9bb8c7e8ede9bf3a18
shubdev <shub.mahadule@gmail.com>
2026-04-08 23:59:41 +0530

    solved que

--- Que43.js
+++ Que43.js
@@ -1,1 +1,30 @@
-//
+//Question 22 using Switch (Check Consonant or Vowel using Switch)
+
+function checkVowelOrConsonant(char) {
+    switch (char.toLowerCase()) {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+            console.log(`${char} is a vowel.`);
+            break;
+        default:
+            if (char.match(/^[a-z]$/i)) {
+                console.log(`${char} is a consonant.`);
+            }
+            else {
+                console.log(`${char} is not an alphabet.`);
+            }
+            
+    }
+}
+// Example usage:
+checkVowelOrConsonant('a');
+checkVowelOrConsonant('b');
+checkVowelOrConsonant('E');
+checkVowelOrConsonant('z');
+checkVowelOrConsonant('1');
+checkVowelOrConsonant('@');
+
+
