273ce83c69bc1adfc5ba537e7efd47a9ff692279
shubdev <shub.mahadule@gmail.com>
2026-04-13 23:58:22 +0530

    solve qes48

---
+++ Que49.js
@@ -0,0 +1,13 @@
+// Inverted Right Triangle Pattern
+
+let n = 5;
+
+for (let i = n; i >= 1; i--) {
+  let row = "";
+
+  for (let j = 1; j <= i; j++) {
+    row += j;
+  }
+
+  console.log(row);
+}
