cad3da0141e379d686aec653cae53ba2879ac4a4
shubdev <shub.mahadule@gmail.com>
2026-04-14 22:40:00 +0530

    solved

---
+++ Que51.js
@@ -0,0 +1,15 @@
+//Triangle Pattern
+
+
+function triangle(n) {
+
+    for (let i = 1; i <= n; i++) {
+        let str = "";
+        for (let j = 1; j <= i; j++) {
+            str += "*";
+
+        }
+        console.log(str);
+    }
+}
+triangle(5);
